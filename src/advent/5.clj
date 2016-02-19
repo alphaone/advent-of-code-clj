ea9f9368b84d72e234b717203703dc7feaf01c5c
Torsten Mangner <tmangner@inoxio.de>
2016-02-19 22:46:35 +0100

    clean up day 5

--- src/advent/5.clj
+++ src/advent/5.clj
@@ -1,25 +1,21 @@
 (ns advent.5)
 
-(defn vowel? [c]
-  (some #(= c %) "aeiou"))
+(defn vowel? [ch]
+  (some #(= ch %) "aeiou"))
 
 (defn three-vowels? [input]
-  (->> input
-       (filter vowel?)
+  (->> (filter vowel? input)
        count
        (<= 3)))
 
 (defn double-letter [input]
-  (not (nil? (re-find #"(\w)\1" input))))
-
-(defn substring? [sub st]
-  (not= (.indexOf st sub) -1))
+  (some? (re-find #"(\w)\1" input)))
 
 (defn naughty-things [input]
-  (or (substring? "ab" input)
-      (substring? "cd" input)
-      (substring? "pq" input)
-      (substring? "xy" input)))
+  (or (clojure.string/includes? input "ab")
+      (clojure.string/includes? input "cd")
+      (clojure.string/includes? input "pq")
+      (clojure.string/includes? input "xy")))
 
 (defn nice? [input]
   (and (three-vowels? input)
@@ -27,23 +23,22 @@
        (not (naughty-things input))))
 
 (defn count-nice-words [input]
-  (let [words (clojure.string/split input #"\n")]
-    (->> words
-         (filter nice?)
-         (count))))
+  (->> (clojure.string/split input #"\n")
+       (filter nice?)
+       count))
+
 
 (defn any-two-letter-twice [input]
-  (not (nil? (re-find #"(\w{2,}).*\1" input))))
+  (some? (re-find #"(\w{2,}).*\1" input)))
 
 (defn repeats-with-exactly-one-letter-between [input]
-  (not (nil? (re-find #"(\w).\1" input))))
+  (some? (re-find #"(\w).\1" input)))
 
 (defn really-nice? [input]
   (and (any-two-letter-twice input)
        (repeats-with-exactly-one-letter-between input)))
 
 (defn count-really-nice-words [input]
-  (let [words (clojure.string/split input #"\n")]
-    (->> words
-         (filter really-nice?)
-         (count))))
+  (->> (clojure.string/split input #"\n")
+       (filter really-nice?)
+       count))
