e8bb3d08a1de58b6f7c0372496f54ebaf213d69e
Torsten Mangner <tmangner@inoxio.de>
2016-02-19 22:38:49 +0100

    clean up day 4

--- src/advent/4.clj
+++ src/advent/4.clj
@@ -11,9 +11,11 @@
         padding (apply str (repeat (- size (count sig)) "0"))]
     (str padding sig)))
 
-(defn is-coin? [no-of-zeros secret i]
-  (let [adv-coin (str secret i)]
-    (.startsWith (md5 adv-coin) (apply str (repeat no-of-zeros "0")))))
+(defn coin? [search-pattern secret i]
+  (let [advent-coin (str secret i)]
+    (clojure.string/starts-with? (md5 advent-coin) search-pattern)))
 
 (defn advent-coins [no-of-zeros secret]
-  (first (filter (partial is-coin? no-of-zeros secret) (range 1e7))))
+  (let [search-pattern (apply str (repeat no-of-zeros "0"))]
+    (->> (filter (partial coin? search-pattern secret) (range 1e7))
+         (first))))
