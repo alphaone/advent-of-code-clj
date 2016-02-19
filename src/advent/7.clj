30a14953c528e026e86b015be4df70b478ff7bae
Torsten Mangner <tmangner@inoxio.de>
2016-02-20 00:10:59 +0100

    clean-up day 7

--- src/advent/7.clj
+++ src/advent/7.clj
@@ -4,30 +4,30 @@
 (defn parse-and-or [line]
   (let [[x action y res]
         (take-last 4
-          (re-find #"(\w+) (AND|OR) (\w+) -> (\w+)" line))]
+                   (re-find #"(\w+) (AND|OR) (\w+) -> (\w+)" line))]
     [(keyword res)
      [(keyword action) (keyword x) (keyword y)]]))
 
 (defn parse-shift [line]
   (let [[x action y res]
         (take-last 4
-          (re-find #"(\w+) (LSHIFT|RSHIFT) (\d+) -> (\w+)" line))]
+                   (re-find #"(\w+) (LSHIFT|RSHIFT) (\d+) -> (\w+)" line))]
     [(keyword res)
      [(keyword action) (keyword x) (read-string y)]]))
 
 (defn parse-not [line]
   (let [[x res]
         (take-last 2
-          (re-find #"NOT (\w+) -> (\w+)" line))]
+                   (re-find #"NOT (\w+) -> (\w+)" line))]
     [(keyword res)
      [:NOT (keyword x)]]))
 
 (defn parse-direct [line]
   (let [[x res]
         (take-last 2
-          (re-find #"(\w+) -> (\w+)" line))]
-   [(keyword res)
-    [:DIRECT (keyword x)]]))
+                   (re-find #"(\w+) -> (\w+)" line))]
+    [(keyword res)
+     [:DIRECT (keyword x)]]))
 
 (defn parse-line [line]
   (cond
@@ -44,20 +44,21 @@
 (declare evaluate-memo)
 
 (defn evaluate [cache wire-map key]
-  (let [v (get wire-map key)]
-    (if v
-      (case (first v)
-        :DIRECT (evaluate-memo cache wire-map (second v))
-        :LSHIFT (bit-shift-left (evaluate-memo cache wire-map (second v)) (get v 2))
-        :RSHIFT (bit-shift-right (evaluate-memo cache wire-map (second v)) (get v 2))
-        :AND    (bit-and (evaluate-memo cache wire-map (second v)) (evaluate-memo cache wire-map (get v 2)))
-        :OR     (bit-or (evaluate-memo cache wire-map (second v)) (evaluate-memo cache wire-map (get v 2)))
-        :NOT    (bit-not (evaluate-memo cache wire-map (second v))))
+  (let [[action a b] (get wire-map key)]
+    (if action
+      (case action
+        :DIRECT (evaluate-memo cache wire-map a)
+        :LSHIFT (bit-shift-left (evaluate-memo cache wire-map a) b)
+        :RSHIFT (bit-shift-right (evaluate-memo cache wire-map a) b)
+        :AND (bit-and (evaluate-memo cache wire-map a) (evaluate-memo cache wire-map b))
+        :OR (bit-or (evaluate-memo cache wire-map a) (evaluate-memo cache wire-map b))
+        :NOT (bit-not (evaluate-memo cache wire-map a)))
       (read-string (name key)))))
 
 (defn evaluate-memo [cache wire-map key]
   (if (cache/has? @cache key)
-      (get @cache key)
-      (let [v (evaluate cache wire-map key)]
-        (swap! cache #(cache/miss % key v))
-        v)))
+    (get @cache key)
+    (let [v (evaluate cache wire-map key)]
+      (swap! cache #(cache/miss % key v))
+      v)))
+
