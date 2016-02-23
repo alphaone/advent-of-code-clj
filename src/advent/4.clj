579972cbade51cb3fbdb695fe553c59308013200
Torsten Mangner <tmangner@inoxio.de>
2016-02-23 16:18:08 +0100

    use digest/md5 instead of copied md5-snippet

--- src/advent/4.clj
+++ src/advent/4.clj
@@ -1,19 +1,9 @@
-(ns advent.4)
-
-(import 'java.security.MessageDigest
-        'java.math.BigInteger)
-
-(defn md5 [s]
-  (let [algorithm (MessageDigest/getInstance "MD5")
-        size (* 2 (.getDigestLength algorithm))
-        raw (.digest algorithm (.getBytes s))
-        sig (.toString (BigInteger. 1 raw) 16)
-        padding (apply str (repeat (- size (count sig)) "0"))]
-    (str padding sig)))
+(ns advent.4
+  (:require [digest]))
 
 (defn coin? [search-pattern secret i]
   (let [advent-coin (str secret i)]
-    (clojure.string/starts-with? (md5 advent-coin) search-pattern)))
+    (clojure.string/starts-with? (digest/md5 advent-coin) search-pattern)))
 
 (defn advent-coins [no-of-zeros secret]
   (let [search-pattern (apply str (repeat no-of-zeros "0"))]
