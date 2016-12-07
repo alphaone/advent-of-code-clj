bbb64a1a2c4d35cfe5a46fd7e63999cfa1d82d00
Torsten Mangner <tmangner@inoxio.de>
2016-12-07 09:21:10 +0100

    Day 5 Task 2

--- src/advent2016/dec5.clj
+++ src/advent2016/dec5.clj
@@ -12,12 +12,39 @@
        (map md5)
        (filter #(cs/starts-with? % "00000"))))
 
-(defn fifth-letter [word]
+(defn sixth-letter [word]
   (-> word (nth 5) (str)))
 
-(defn password [md5-hashes]
+(defn password-a [md5-hashes]
   (->> md5-hashes
-       (map fifth-letter)
+       (map sixth-letter)
        (take 8)
        (cs/join)))
+
+(defn sixth-and-seventh-letter [word]
+  (let [[pos ch] (->> [5 6]
+                      (map #(nth word %))
+                      (map str))]
+    [(read-string pos) ch]))
+
+(defn has-position? [[pos _]]
+  (and (number? pos) (<= 0 pos 7)))
+
+(defn complete-pwd? [pwd]
+  (not (cs/includes? pwd "_")))
+
+(defn build-password [password [pos ch]]
+  (let [pwd (vec password)]
+    (if (= \_ (nth pwd pos))
+      (-> pwd (assoc pos ch) (cs/join))
+      password)))
+
+(defn password-b [md5-hashes]
+  (->> md5-hashes
+       (map sixth-and-seventh-letter)
+       (filter has-position?)
+       (reductions build-password "________")
+       (filter complete-pwd?)
+       (first))
+  )
 
