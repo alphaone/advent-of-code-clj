0d4f4b9ca289ad61770857268293d22ccc9367f2
Torsten Mangner <tmangner@inoxio.de>
2017-12-14 20:04:38 +0100

    Day 7

--- src/advent2017/dec7.clj
+++ src/advent2017/dec7.clj
@@ -6,15 +6,48 @@
         holding (if h (cs/split h #", ") [])]
     {:name    name
      :weight  (Integer/parseInt weight)
-     :holding holding})
-  )
+     :holding holding}))
 
-(defn find-root [input]
-  (let [programs (->> (cs/split-lines input)
-                      (map parse-line))
-        all-names (mapcat (fn [p] (:holding p)) programs)]
+(defn all-programs [input]
+  (->> (cs/split-lines input)
+       (map parse-line)))
+
+(defn named [ps]
+  (into {} (map (fn [p] [(:name p) p]) ps)))
+
+(defn find-root [programs]
+  (let [all-names (mapcat (fn [p] (:holding p)) programs)]
     (->> programs
          (remove (fn [p] (contains? (set all-names) (:name p))))
          first
          :name)))
 
+(defn descendants [all-nodes node-id]
+  (get-in all-nodes [node-id :holding]))
+
+(defn ->branch [node kids]
+  (assoc node
+    :holding kids
+    :cum-weight (reduce + (conj (map :cum-weight kids) (:weight node)))))
+
+(defn ->tree [all-nodes node-id]
+  (let [node (get all-nodes node-id)]
+    (->branch node (map (partial ->tree all-nodes)
+                        (descendants all-nodes node-id)))))
+
+(defn balanced? [key nodes]
+  (prn "balanced" (map key nodes))
+  (= 1 (count (frequencies (map key nodes)))))
+
+(defn find-unbalanced [key nodes]
+  (let [fs  (frequencies (map key nodes))
+        least-freq-weight (first (reduce (fn [[mw mf] [w f]] (if (< f mf) [w f] [mw mf])) [-1 100] fs))]
+    (first (filter (fn [n] (= least-freq-weight (key n))) nodes))))
+
+(defn find-wrong-weight [siblings node]
+  (prn "find-wrong-weight" (map :cum-weight (:holding node)))
+  (let [kids (:holding node)]
+    (if (balanced? :cum-weight kids)
+      {:wrong-weight-node (select-keys node [:weight :cum-weight])
+       :siblings          (map :cum-weight siblings)}
+      (find-wrong-weight kids (find-unbalanced :cum-weight kids)))))
--- test/advent2017/dec7_test.clj
+++ test/advent2017/dec7_test.clj
@@ -5,16 +5,64 @@
 
 (deftest parse-line-test
   (testing "it parses a line"
-    (is (= {:name "pbga" 
-            :weight 66 
+    (is (= {:name    "pbga"
+            :weight  66
             :holding []}
            (a/parse-line "pbga (66)")))
-    (is (= {:name "fwft" 
-            :weight 72 
+    (is (= {:name    "fwft"
+            :weight  72
             :holding ["ktlj" "cntj" "xhth"]}
            (a/parse-line "fwft (72) -> ktlj, cntj, xhth")))))
 
 (deftest solve-a-test
   (testing "it finds the root node"
-    (is (=  "hlqnsbe"
-            (a/find-root (slurp (io/resource "dec7.txt")))))))
+    (is (= "hlqnsbe"
+           (-> (slurp (io/resource "dec7.txt"))
+               (a/all-programs)
+               (a/find-root))))))
+
+(deftest tree-test
+  (testing "it should"
+    (is (= {:holding [{:holding []
+                       :name    "abc"
+                       :weight  1
+                       :cum-weight  1}
+                      {:holding []
+                       :name    "def"
+                       :weight  2
+                       :cum-weight  2}]
+            :name    "ghi"
+            :weight  4
+            :cum-weight  7}
+           (a/->tree {"abc" {:name    "abc"
+                             :weight  1
+                             :holding []}
+                      "def" {:name    "def"
+                             :weight  2
+                             :holding []}
+                      "ghi" {:name    "ghi"
+                             :weight  4
+                             :holding ["abc" "def"]}}
+                     "ghi")))))
+
+(deftest balanced?-test
+  (testing "it should"
+    (is (= false (a/balanced?  :w [{:w 10} {:w 10} {:w 10} {:w 9}])))
+    (is (= true (a/balanced?  :w [{:w 10} {:w 10} {:w 10} {:w 10}])))))
+
+(deftest find-unbalanced-value-test
+  (testing "it should"
+    (is (= {:w 9}
+           (a/find-unbalanced  :w [{:w 10} {:w 10} {:w 10} {:w 9}])))))
+
+(deftest find-wrong-weight-test
+  (testing "it should"
+    (testing "it should"
+      (let [tree (a/->tree (-> (slurp (io/resource "dec7.txt"))
+                               (a/all-programs)
+                               (a/named))
+                           "hlqnsbe")]
+        (is (= {:siblings          (2102 2097 2097 2097 2097)
+                :wrong-weight-node {:cum-weight 2102
+                                    :weight     1998}}
+               (a/find-wrong-weight [] tree)))))))
