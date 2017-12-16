0673d4cb8d5b53bf80e670c4990eb7a6858956ae
Torsten Mangner <tmangner@inoxio.de>
2017-12-16 14:16:13 +0100

    So much better version of Day3

--- src/advent2017/dec3.clj
+++ src/advent2017/dec3.clj
@@ -1,29 +1,45 @@
 (ns advent2017.dec3)
 
-(defn edge-length->shell-no [x]
-  (loop [acc 0
-         current x]
-    (if (= current 1)
-      acc
-      (recur (inc acc) (dec (dec current))))))
+(defn go-y [direction shell-no {:keys [x y]}]
+  (let [steps (range 1 (inc (* 2 shell-no)))]
+    (map (fn [i] {:x x :y (direction y i)}) steps)))
 
-(defn edge-length [x]
-  (let [s   (Math/sqrt x)
-        s'  (int (Math/ceil s))
-        s'' (if (odd? s') s' (inc s'))]
-    s''))
+(defn go-x [direction shell-no {:keys [x y]}]
+  (let [steps (range 1 (inc (* 2 shell-no)))]
+    (map (fn [i] {:x (direction x i) :y y}) steps)))
 
-(defn shell-no [x]
-  (edge-length->shell-no (edge-length x)))
+(defn coords-for-shell [shell-no]
+  (if (= 0 shell-no)
+    [{:x 0 :y 0}]
+    (let [pseudo-start {:x shell-no :y (* -1 shell-no)}
+          to-north     (go-y + shell-no pseudo-start)
+          to-west      (go-x - shell-no (last to-north))
+          to-south     (go-y - shell-no (last to-west))
+          to-east      (go-x + shell-no (last to-south))]
+      (concat to-north to-west to-south to-east))))
 
-(defn sqr [a] (* a a))
+(defn all-coords []
+  (mapcat coords-for-shell (range)))
 
-(defn dist-to-axis [x]
-  (let [el  (edge-length x)
-        corner (sqr el)
-        dist (- corner x)]
-    (Math/abs (- (mod dist (dec el)) (/ (dec el) 2))))
-  )
+(defn manhatten-dist [c]
+  (+ (Math/abs (:x c))
+     (Math/abs (:y c))))
 
-(defn route-length [x]
-  (+ (shell-no x) (dist-to-axis x)))
+(defn neighbor-coods [{:keys [x y]}]
+  (for [x' (range (dec x) (+ x 2))
+        y' (range (dec y) (+ y 2))
+        :when (not (and (= x x') (= y y')))]
+    {:x x' :y y'}))
+
+(defn neighbor-sum [adj-sums coord]
+  (->> (neighbor-coods coord)
+       (select-keys adj-sums)
+       (vals)
+       (reduce +)))
+
+(defn step [stop adj-sums coord]
+  (let [value (neighbor-sum adj-sums coord)]
+    (if (> value stop)
+      (reduced value)
+      (assoc adj-sums coord value))))
+
--- test/advent2017/dec3_test.clj
+++ test/advent2017/dec3_test.clj
@@ -2,47 +2,50 @@
   (:require [clojure.test :refer :all]
             [advent2017.dec3 :as a]))
 
-(deftest edge->shell-no-test
-  (testing "it should"
-    (is (= 0 (a/edge-length->shell-no 1)))
-    (is (= 1 (a/edge-length->shell-no 3)))))
-
-(deftest shell-no-test
-  (testing "it should calc shell number for a given int"
-    (is (= 0 (a/shell-no 1)))
-    (is (= 1 (a/shell-no 2)))
-    (is (= 1 (a/shell-no 3)))
-    (is (= 1 (a/shell-no 9)))
-    (is (= 2 (a/shell-no 10)))))
-
-(deftest dist-to-axis-test
-  (testing "it should"
-    (is (= 0 (a/dist-to-axis 2)))
-    (is (= 1 (a/dist-to-axis 3)))
-    (is (= 0 (a/dist-to-axis 4)))
-
-    (is (= 0 (a/dist-to-axis 11)))
-    (is (= 1 (a/dist-to-axis 12)))
-    (is (= 2 (a/dist-to-axis 13)))
-    (is (= 1 (a/dist-to-axis 14)))
-    (is (= 0 (a/dist-to-axis 15)))
-    (is (= 1 (a/dist-to-axis 16)))
-    (is (= 2 (a/dist-to-axis 17)))
-    (is (= 1 (a/dist-to-axis 18)))
-    
-    (is (= 2 (a/dist-to-axis 21)))
-    (is (= 1 (a/dist-to-axis 22)))
-    (is (= 0 (a/dist-to-axis 23)))
-    (is (= 1 (a/dist-to-axis 24)))
-    (is (= 2 (a/dist-to-axis 25)))))
-
-(deftest route-length-test
-  (testing "it calc the route length"
-    (is (= 1 (a/route-length 2)))
-    (is (= 3 (a/route-length 20)))
-    (is (= 5 (a/route-length 26)))))
+(deftest coords-for-shell-test
+  (testing "it calcs all (ordered) coords of one shell"
+    (is (= [{:x 0 :y 0}] (a/coords-for-shell 0)))
+    (is (= [{:x 1 :y 0} {:x 1 :y 1}
+            {:x 0 :y 1} {:x -1 :y 1}
+            {:x -1 :y 0} {:x -1 :y -1}
+            {:x 0 :y -1} {:x 1 :y -1}]
+           (a/coords-for-shell 1)))
+    (is (= [{:x 2 :y -1} {:x 2 :y 0} {:x 2 :y 1} {:x 2 :y 2}
+            {:x 1 :y 2} {:x 0 :y 2} {:x -1 :y 2} {:x -2 :y 2}
+            {:x -2 :y 1} {:x -2 :y 0} {:x -2 :y -1} {:x -2 :y -2}
+            {:x -1 :y -2} {:x 0 :y -2} {:x 1 :y -2} {:x 2 :y -2}]
+           (a/coords-for-shell 2)))))
 
 (deftest solve-a-test
-  (testing "it should"
+  (testing "it finds the given position and returns its distance"
     (is (= 371
-           (a/route-length 368078)))))
+           (->> (a/all-coords)
+                (drop (dec 368078))
+                first
+                a/manhatten-dist)))))
+
+(deftest neighbor-coords-test
+  (testing "it finds the neighbors of a coord"
+    (is (= [{:x -1 :y -1} {:x -1 :y 0} {:x -1 :y 1}
+            {:x 0 :y -1} {:x 0 :y 1}
+            {:x 1 :y -1} {:x 1 :y 0} {:x 1 :y 1}]
+           (a/neighbor-coods {:x 0 :y 0})))))
+
+(deftest step-test
+  (testing "it calcs a new version of the adj-sums map"
+    (is (= {{:x 0 :y 0} 1
+            {:x 1 :y 0} 1}
+           (a/step 1000 {{:x 0 :y 0} 1}
+                   {:x 1 :y 0})))
+    (is (= {{:x 0 :y 0} 1
+            {:x 1 :y 0} 1
+            {:x 1 :y 1} 2}
+           (a/step 1000 {{:x 0 :y 0} 1
+                         {:x 1 :y 0} 1}
+                   {:x 1 :y 1})))))
+(deftest solve-b-test
+  (testing "it finds the first sum of input"
+    (is (= 369601
+           (reduce (partial a/step 368078)
+                   {{:x 0 :y 0} 1}
+                   (drop 1 (a/all-coords)))))))
