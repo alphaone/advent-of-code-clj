9cd90d175d19c11126646eae4e37967ed31da705
Torsten Mangner <tmangner@inoxio.de>
2019-01-27 16:26:17 +0100

    Less printing

--- 2018/src/advent2018/dec8.clj
+++ 2018/src/advent2018/dec8.clj
@@ -1,11 +1,8 @@
 (ns advent2018.dec8)
 
 (defn sum-meta-entries [acc [f s & rest :as input]]
-  (when (> 100 (count input)) (prn acc input))
   (if (= f 0)
-    (do
-      (prn "A" )
-      [(drop s rest) (reduce + acc (take s rest))])
-    (let [[rest' sum] (do (prn "V") (sum-meta-entries 0 rest))]
+    [(drop s rest) (reduce + acc (take s rest))]
+    (let [[rest' sum] (sum-meta-entries 0 rest)]
       (sum-meta-entries (+ acc sum) (concat [(dec f) s] rest')))
     ))
