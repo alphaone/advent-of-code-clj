b0978d341af5976e5d3fe10fa0a62837ee1614b8
Torsten Mangner <tmangner@inoxio.de>
2019-04-16 10:06:14 +0200

    Solve part 2 of day 11

--- 2018/src/advent2018/dec11.clj
+++ 2018/src/advent2018/dec11.clj
@@ -15,22 +15,28 @@
         (hundreds-digit)
         (- 5))))
 
-(defn square-power-level [serial-number [x-start y-start]]
-  (->> (for [x (range x-start (+ x-start 3))
-             y (range y-start (+ y-start 3))]
+(defn square-power-level [serial-number size [x-start y-start]]
+  (->> (for [x (range x-start (+ x-start size))
+             y (range y-start (+ y-start size))]
          (power-level serial-number [x y]))
        (reduce +)))
 
-(defn find-max-power-level [serial-number {:keys [max-power-level] :as acc} [x y]]
-  (let [power-level (square-power-level serial-number [x y])]
+(defn find-max-power-level [serial-number size {:keys [max-power-level] :as acc} [x y]]
+  (let [power-level (square-power-level serial-number size [x y])]
     (if (> power-level max-power-level)
-      {:max-power-level power-level
-       :best-coords     [x y]}
+      (do (println "Found something good:" size "->" power-level "(" x y ")")
+          {:max-power-level power-level
+           :best-coords     [x y]
+           :size            size})
       acc)))
 
+(defn best-square-of-size [serial-number {:keys [:max-power-level] :as acc} size]
+  (->> (for [x (range 1 (- 301 (dec size))) 
+             y (range 1 (- 301 (dec size)))] 
+         [x y])
+       (reduce (partial find-max-power-level serial-number size) acc)))
+
 (defn best-square [serial-number]
-  (->> (for [x (range 1 299) y (range 1 299)] [x y])
-       (reduce (partial find-max-power-level serial-number)
-               {:max-power-level 0
-                :best-coords     [-1 -1]})
-       :best-coords))
+  (->> (range 1 20)
+       (reduce (partial best-square-of-size serial-number)
+               {:max-power-level 0})))
--- 2018/test/advent2018/dec11_test.clj
+++ 2018/test/advent2018/dec11_test.clj
@@ -11,7 +11,7 @@
 
 (deftest square-power-level-test
   (testing "it sum up the power levels in a 3x3 square"
-    (is (= 29 (a/square-power-level 18 [33 45])))))
+    (is (= 29 (a/square-power-level 18 3 [33 45])))))
 
 (deftest rack-id-test
   (is (= 13 (a/rack-id 3))))
@@ -24,15 +24,38 @@
   (is (= {:max-power-level 29
           :best-coords     [33 45]}
          (a/find-max-power-level 18
+                                 3
                                  {:max-power-level 10
                                   :best-coords     [9 9]} [33 45])))
   (is (= {:max-power-level 30
           :best-coords     [9 9]}
          (a/find-max-power-level 18
+                                 3
                                  {:max-power-level 30
                                   :best-coords     [9 9]} [33 45]))))
 
+(deftest best-square-of-size-test
+  (testing "it gets the square (of given size) with the max power level"
+    (is (= {:best-coords     [33 45]
+            :max-power-level 29
+            :size 3}
+           (a/best-square-of-size 18 {:max-power-level 0} 3)))
+    (is (= {:best-coords     [90 269]
+            :max-power-level 113
+            :size 16}
+           (a/best-square-of-size 18 {:max-power-level 0} 16)))
+    (is (= {:best-coords     [20 46]
+            :max-power-level 30
+            :size 3}
+           (a/best-square-of-size 4151 {:max-power-level 0} 3)))))
+
 (deftest best-square-test
   (testing "it gets the square with the max power level"
-    (is (= [33 45] (a/best-square 18)))
-    (is (= [20 46] (a/best-square 4151)))))
+    (is (= {:best-coords [90 269]
+            :max-power-level 113
+            :size        16}
+           (time (a/best-square 18))))
+    (is (= {:best-coords [231 65]
+            :max-power-level 158
+            :size        14}
+           (time (a/best-square 4151))))))
