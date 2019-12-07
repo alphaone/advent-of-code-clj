66f3ab9c399b15e7ad2a12dbe65c5921009decce
Torsten Mangner <tmangner@inoxio.de>
2019-12-07 13:41:47 +0100

    Solve 2019 day3 part 2

--- 2019/src/advent2019/dec3.clj
+++ 2019/src/advent2019/dec3.clj
@@ -10,10 +10,13 @@
 (def which-coord {"R" 0 "L" 0 "U" 1 "D" 1})
 
 (defn operation [cmd]
-  (let [[dir amount] (parse-command cmd)
+  (let [[dir steps] (parse-command cmd)
         op     (ops dir)
         x-or-y (which-coord dir)]
-    [#(update % x-or-y op) amount]))
+    [#(update % x-or-y op) steps]))
+
+(defn steps [cmd]
+  (second (parse-command cmd)))
 
 (defn path [current-pos cmd]
   (let [[op amount] (operation cmd)]
@@ -25,38 +28,55 @@
   (-> (path current-pos cmd)
       last))
 
-(defn mark-board [current wire-id]
-  (bit-or current wire-id))
+(defn mark-board [cell step wire-id]
+  (update cell wire-id (fn [historic] (or historic step))))
 
-(defn lay-wire [wire-id board pos]
-  (update board pos (fnil mark-board 0) wire-id))
+(defn lay-wire [step wire-id board idx pos]
+  (update board pos mark-board (+ step (inc idx)) wire-id))
 
-(defn single-wire [board current-pos wire-id cmd]
-  (let [path (path current-pos cmd)]
-    (reduce (partial lay-wire wire-id) board path)))
+(defn single-wire [board current-pos step wire-id cmd]
+  (let [path (vec (path current-pos cmd))]
+    (reduce-kv (partial lay-wire step wire-id) board path)))
 
-(defn wire [board current-pos wire-id cmds]
+(defn wire [board current-pos step wire-id cmds]
   (if (empty? cmds)
     board
     (let [cmd    (first cmds)
-          board' (single-wire board current-pos wire-id cmd)
-          pos'   (move current-pos cmd)]
-      (recur board' pos' wire-id (rest cmds)))))
+          board' (single-wire board current-pos step wire-id cmd)
+          pos'   (move current-pos cmd)
+          step'  (+ step (steps cmd))]
+      (recur board' pos' step' wire-id (rest cmds)))))
 
 (defn find-nearest-crossings [board]
   (->> board
-       (filter #(= (val %) 3))
+       (filter (fn [[_ cell]] (= (keys cell) [1 2])))
        keys
        (sort-by (fn [[x y]] (+ (Math/abs x) (Math/abs y))))
        first))
 
+(defn fewest-steps-to-crossings [board]
+  (->> board
+       (filter (fn [[_ cell]] (= (keys cell) [1 2])))
+       (sort-by (fn [[_ cell]] (reduce + (vals cell))))
+       first))
+
+(defn puzzle-input []
+  (->> "dec3.txt"
+       (io/resource)
+       (slurp)
+       (cs/split-lines)
+       (map (fn [s] (cs/split s #",")))))
+
 (defn solve-part1 []
-  (let [[path1 path2] (->> "dec3.txt"
-                          (io/resource)
-                          (slurp)
-                          (cs/split-lines)
-                          (map (fn [s] (cs/split s #","))))]
+  (let [[path1 path2] (puzzle-input)]
     (-> {}
-        (wire [0 0] 1 (vec path1))
-        (wire [0 0] 2 (vec path2))
+        (wire [0 0] 0 1 (vec path1))
+        (wire [0 0] 0 2 (vec path2))
         (find-nearest-crossings))))
+
+(defn solve-part2 []
+  (let [[path1 path2] (puzzle-input)]
+    (-> {}
+        (wire [0 0] 0 1 (vec path1))
+        (wire [0 0] 0 2 (vec path2))
+        (fewest-steps-to-crossings))))
--- 2019/test/advent2019/dec3_test.clj
+++ 2019/test/advent2019/dec3_test.clj
@@ -20,63 +20,93 @@
 
 (deftest single-wire-test
   (testing "it lays the wire"
-    (is (= {[1 0] 1
-            [2 0] 1}
-           (d/single-wire {} [0 0] 1 "R2"))))
+    (is (= {[1 0] {1 1}
+            [2 0] {1 2}}
+           (d/single-wire {} [0 0] 0 1 "R2"))))
   (testing "it will ignore crossing of same wire"
-    (is (= {[1 0] 1
-            [2 0] 1}
-           (d/single-wire {[1 0] 1}
-                          [0 0] 1 "R2"))))
+    (is (= {[1 0] {1 3}
+            [2 0] {1 12}}
+           (d/single-wire {[1 0] {1 3}}
+                          [0 0] 10 1 "R2"))))
   (testing "it marks crossings of different wires"
-    (is (= {[1 0] 3
-            [2 0] 1}
-           (d/single-wire {[1 0] 2} [0 0] 1 "R2")))))
+    (is (= {[1 0] {1 1 2 1}
+            [2 0] {1 2}}
+           (d/single-wire {[1 0] {2 1}} [0 0] 0 1 "R2")))))
 
 (deftest wire-test
   (testing "it lays the wire along the given paths"
-    (is (= {[1 0] 1
-            [2 0] 1
-            [3 0] 1
-            [4 0] 1
-            [5 0] 1
-            [6 0] 1
-            [7 0] 1
-            [8 0] 1
-            [8 1] 1
-            [8 2] 1
-            [8 3] 1
-            [8 4] 1
-            [8 5] 1
-            [7 5] 1
-            [6 5] 1
-            [5 5] 1
-            [4 5] 1
-            [3 5] 1
-            [3 4] 1
-            [3 3] 1
-            [3 2] 1}
-           (d/wire {} [0 0] 1 ["R8" "U5" "L5" "D3"])))))
+    (is (= {[1 0] {1 1}
+            [2 0] {1 2}
+            [3 0] {1 3}
+            [4 0] {1 4}
+            [5 0] {1 5}
+            [6 0] {1 6}
+            [7 0] {1 7}
+            [8 0] {1 8}
+            [8 1] {1 9}
+            [8 2] {1 10}
+            [8 3] {1 11}
+            [8 4] {1 12}
+            [8 5] {1 13}
+            [7 5] {1 14}
+            [6 5] {1 15}
+            [5 5] {1 16}
+            [4 5] {1 17}
+            [3 5] {1 18}
+            [3 4] {1 19}
+            [3 3] {1 20}
+            [3 2] {1 21}}
+           (d/wire {} [0 0] 0 1 ["R8" "U5" "L5" "D3"])))))
 
-(deftest crossing-test
+(deftest find-nearest-crossings
   (testing "it finds crossings"
     (is (= [3 3]
            (-> {}
-               (d/wire [0 0] 1 ["R8" "U5" "L5" "D3"])
-               (d/wire [0 0] 2 ["U7" "R6" "D4" "L4"])
+               (d/wire [0 0] 0 1 ["R8" "U5" "L5" "D3"])
+               (d/wire [0 0] 0 2 ["U7" "R6" "D4" "L4"])
                (d/find-nearest-crossings))))
     (is (= [155 4]
            (-> {}
-               (d/wire [0 0] 1 ["R75" "D30" "R83" "U83" "L12" "D49" "R71" "U7" "L72"])
-               (d/wire [0 0] 2 ["U62" "R66" "U55" "R34" "D71" "R55" "D58" "R83"])
+               (d/wire [0 0] 0 1 ["R75" "D30" "R83" "U83" "L12" "D49" "R71" "U7" "L72"])
+               (d/wire [0 0] 0 2 ["U62" "R66" "U55" "R34" "D71" "R55" "D58" "R83"])
                (d/find-nearest-crossings))))
     (is (= [124 11]
            (-> {}
-               (d/wire [0 0] 1 ["R98" "U47" "R26" "D63" "R33" "U87" "L62" "D20" "R33" "U53" "R51"])
-               (d/wire [0 0] 2 ["U98" "R91" "D20" "R16" "D67" "R40" "U7" "R15" "U6" "R7"])
+               (d/wire [0 0] 0 1 ["R98" "U47" "R26" "D63" "R33" "U87" "L62" "D20" "R33" "U53" "R51"])
+               (d/wire [0 0] 0 2 ["U98" "R91" "D20" "R16" "D67" "R40" "U7" "R15" "U6" "R7"])
                (d/find-nearest-crossings))))))
+
+(deftest fewest-steps-to-crossings
+  (testing "it finds crossings"
+    (is (= [[6 5]
+            {1 15 2 15}]
+           (-> {}
+               (d/wire [0 0] 0 1 ["R8" "U5" "L5" "D3"])
+               (d/wire [0 0] 0 2 ["U7" "R6" "D4" "L4"])
+               (d/fewest-steps-to-crossings))))
+    (is (= [[158 -12]
+            {1 206
+             2 404}]
+           (-> {}
+               (d/wire [0 0] 0 1 ["R75" "D30" "R83" "U83" "L12" "D49" "R71" "U7" "L72"])
+               (d/wire [0 0] 0 2 ["U62" "R66" "U55" "R34" "D71" "R55" "D58" "R83"])
+               (d/fewest-steps-to-crossings))))
+    (is (= [[107 47]
+            {1 154
+             2 256}]
+           (-> {}
+               (d/wire [0 0] 0 1 ["R98" "U47" "R26" "D63" "R33" "U87" "L62" "D20" "R33" "U53" "R51"])
+               (d/wire [0 0] 0 2 ["U98" "R91" "D20" "R16" "D67" "R40" "U7" "R15" "U6" "R7"])
+               (d/fewest-steps-to-crossings))))))
 
 (deftest solve-part1-test
   (testing "it solves part 1"
     (is (= [253 63]
            (d/solve-part1)))))
+
+(deftest solve-part2-test
+  (testing "it solves part 2"
+    (is (= [[-586 1149]
+            {1 9055
+             2 7313}]
+           (d/solve-part2)))))
