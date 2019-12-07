182d1123cef066f7f0b1eaf386a878062010d289
Torsten Mangner <tmangner@inoxio.de>
2019-12-07 14:35:20 +0100

    Solve 2019 day4 part 2

--- 2019/src/advent2019/dec4.clj
+++ 2019/src/advent2019/dec4.clj
@@ -1,12 +1,11 @@
 (ns advent2019.dec4)
 
-(def find-double-reexp #"(\d)\1" )
-
-(defn contains-double? [pwd]
-  (re-find find-double-reexp pwd))
+(defn contains-freq-chars? [freq-pred pwd]
+  (some freq-pred (vals (frequencies pwd))))
 
 (defn never-decreases? [pwd]
   (every? (fn [[x y]] (<= (int x) (int y))) (partition 2 1 pwd)))
 
-(defn valid? [pwd]
-  (and (contains-double? pwd) (never-decreases? pwd)))
+(defn valid? [freq-pred pwd]
+  (and (contains-freq-chars? freq-pred pwd)
+       (never-decreases? pwd)))
--- 2019/test/advent2019/dec4_test.clj
+++ 2019/test/advent2019/dec4_test.clj
@@ -4,9 +4,12 @@
 
 (def puzzle-input (range 156218 (inc 652527)))
 
-(deftest contains-double?-test
-  (is (d/contains-double? "122345"))
-  (is (not (d/contains-double? "123789"))))
+(deftest contains-at-least-double?-test
+  (is (d/contains-freq-chars? #(>= % 2) "122345"))
+  (is (not (d/contains-freq-chars? #(>= % 2) "123789")))
+  (is (d/contains-freq-chars? #(= % 2) "112233"))
+  (is (d/contains-freq-chars? #(= % 2) "111122"))
+  (is (not (d/contains-freq-chars? #(= % 2) "123444"))))
 
 (deftest never-decreases?-test
   (is (d/never-decreases? "122345"))
@@ -15,13 +18,20 @@
   (is (not (d/never-decreases? "223450"))))
 
 (deftest valid?-test
-  (is (d/valid? "122345"))
-  (is (not (d/valid? "123789")))
-  (is (not (d/valid? "223450"))))
+  (is (d/valid? #(>= % 2) "122345"))
+  (is (not (d/valid? #(>= % 2) "123789")))
+  (is (not (d/valid? #(>= % 2) "223450"))))
 
 (deftest solve-part1-test
   (is (= 1694
          (->> puzzle-input
               (map str)
-              (filter d/valid?)
+              (filter (partial d/valid? #(>= % 2)))
+              count))))
+
+(deftest solve-part2-test
+  (is (= 1148
+         (->> puzzle-input
+              (map str)
+              (filter (partial d/valid? #(= % 2)))
               count))))
