c2bc052233aeadc7f7edb91dbe8f5bf278e710fb
Torsten Mangner <tmangner@inoxio.de>
2020-12-10 09:03:03 +0100

    Day 10 Part 2 unsolved

--- 2020/src/advent2020/day10.clj
+++ 2020/src/advent2020/day10.clj
@@ -1,18 +1,39 @@
-(ns advent2020.day10)
+(ns advent2020.day10
+  (:require [clojure.string :as str]))
 
 (defn joltage-difference-distribution [input]
-  (let [my-device (+ 3 (apply max input))
-        outlet 0]
-    (->> [outlet my-device]
+  (let [device (+ 3 (apply max input))
+        wall 0]
+    (->> [wall device]
          (concat input)
          (sort)
          (partition 2 1)
          (map (fn [[a b]] (- b a)))
-         (frequencies)))
-  )
+         (frequencies))))
 
 (defn solve-a [input]
   (let [dist (joltage-difference-distribution input)]
     (apply * (map dist ((juxt first last)
                         (sort (keys dist)))))))
+
+(defn valid? [chain]
+  (every? (fn [[a b]] (<= (- b a) 3)) (partition 2 1 chain)))
+
+(defn vec-remove
+  [pos coll]
+  (vec (concat (subvec coll 0 pos) (subvec coll (inc pos)))))
+
+(defn valid-chains [wall device chain]
+  (let [chains (->> (for [i (range (count chain))]
+                      (vec-remove i (vec chain)))
+                    (filter (fn [chain] (valid? (concat [wall] chain [device])))))]
+    (cons chain (mapcat (partial valid-chains wall device) chains))))
+
+(defn solve-b [input]
+  (let [device (+ 3 (apply max input))
+        wall 0
+        sorted (sort input)
+        chains (valid-chains wall device sorted)
+        ]
+    (set (concat [sorted] chains ))))
 
--- 2020/test/advent2020/day10_test.clj
+++ 2020/test/advent2020/day10_test.clj
@@ -6,7 +6,7 @@
 
 (def example-input [16 10 15 5 1 11 7 19 6 12 4])
 (def example-input2 [28 33 18 42 31 14 46 20 48 47 24 23 49 45 19
-                    38 39 11 1 32 25 35 8 17 7 9 4 2 34 10 3])
+                     38 39 11 1 32 25 35 8 17 7 9 4 2 34 10 3])
 
 (deftest joltage-difference-distribution-test
   (is (= {1 7
@@ -24,3 +24,17 @@
 (deftest solve-a
   (is (= 2059
          (day10/solve-a input))))
+
+(deftest valid-test
+  (is (day10/valid? [1 2 3]))
+  (is (day10/valid? [1 4 7]))
+  (is (not (day10/valid? [1 4 8]))))
+
+(deftest solve-b
+  (is (= 8
+         (count (day10/solve-b example-input))))
+  ;; takes already too long :\
+  #_(is (= 19208
+         (count (day10/solve-b example-input2))))
+  )
+
