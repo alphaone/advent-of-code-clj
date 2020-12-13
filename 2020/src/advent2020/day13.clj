73cac42096d376e20ac9b20607ff0860b424504b
Torsten Mangner <tmangner@inoxio.de>
2020-12-13 11:57:53 +0100

    Day 13 Part 2

--- 2020/src/advent2020/day13.clj
+++ 2020/src/advent2020/day13.clj
@@ -11,3 +11,22 @@
              (map (fn [bus-id] [bus-id (next-departure ts bus-id)]))
              (apply min-key second))]
     (* bus-id (- departure ts))))
+
+(defn correct-time-diff? [time bus-b offset-b]
+  (= (mod time bus-b)
+     (- bus-b offset-b)))
+
+; BusA(7| 0)
+; BusB(5| 1)
+;
+; 0---------1---------1---------2---------3---------4
+; 0----5----0----5----0----5----0----5----0----5----0
+; A......A......A......A......A......A......A......A
+; B....B....B....B....B....B....B....B....B....B....B
+;               ^^                                 ^^
+; combined BusAB(35| 14)
+(defn combine-bus [[bus-a offset-a] [bus-b offset-b]]
+  (let [offset (->> (iterate #(+ bus-a %) offset-a)
+                    (drop-while #(not (correct-time-diff? % bus-b offset-b)))
+                    first)]
+    [(* bus-a bus-b) offset]))
--- 2020/test/advent2020/day13_test.clj
+++ 2020/test/advent2020/day13_test.clj
@@ -9,8 +9,7 @@
     [(Long/parseLong line)
      (->> (str/split line2 #",")
           (filter #(re-find #"\d" %))
-          (map #(Long/parseLong %))
-          )]))
+          (map #(Long/parseLong %)))]))
 
 (def input
   (->> (io/resource "day13.txt")
@@ -21,11 +20,44 @@
   (parse "939\n7,13,x,x,59,x,31,19"))
 
 (deftest next-departure-test
-  (is (= 945 (day13/next-departure 939 7) ))
-  (is (= 944 (day13/next-departure 939 59) )))
+  (is (= 945 (day13/next-departure 939 7)))
+  (is (= 944 (day13/next-departure 939 59))))
 
 (deftest solve-a
   (is (= 295
          (day13/earliest-bus example-input)))
   (is (= 2845
          (day13/earliest-bus input))))
+
+(defn parse2 [input]
+  (let [[_ line2] (str/split-lines input)]
+    (->> (str/split line2 #",")
+         (keep-indexed
+           (fn [i x]
+             (when (not= x "x")
+               (let [x (Long/parseLong x)]
+                 [x (mod i x)])))))))
+
+(def input2
+  (->> (io/resource "day13.txt")
+       (slurp)
+       (parse2)
+       (sort-by first)))
+
+(deftest combine-test
+  (is (= [91 77]
+         (day13/combine-bus [7 0] [13 1])))
+  (is (= [1365 896]
+         (reduce day13/combine-bus [[7 0] [13 1] [15 4]]))))
+
+(deftest solve-b
+  (is (= [3162341 1068781]
+         (reduce day13/combine-bus (parse2 "939\n7,13,x,x,59,x,31,19"))))
+  (is (= [4199 3417]
+         (reduce day13/combine-bus (parse2 "\n17,x,13,19"))))
+  (is (= [1687931 1261476]
+         (reduce day13/combine-bus (parse2 "\n67,7,x,59,61"))))
+  (is (= [5876813119 1202161486]
+         (reduce day13/combine-bus (parse2 "\n1789,37,47,1889"))))
+  (is (= [2265213528143033 487905974205117]
+         (reduce day13/combine-bus input2))))
